f4160132337ec57021407b1cef40578c894b1d2d
phapsumoi <vuvancuong2703.qx4@gmail.com>
2025-05-20 14:58:53 +0700

    user auth(login and register)

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import { get } from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config'
 
 
 //app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //middle ware
 app.use(express.json());
 app.use(cors());
+
+app.use('/uploads', express.static('uploads'));
 
 //db connection
 connectDB();
 
 //api endpoints
 app.use("/api/food", foodRouter)
-// app.use("/image", express.static('uploads'))
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 
 app.get('/', (req, res) => {
